ac0e27f4a2b448404082afee98f4e7c46a649fdd
tmm6907 <tmm6907@gmail.com>
2021-04-10 13:53:46 -0400

    Final commit until feature update

--- backend/podcast_finder/spotify_api/views.py
+++ backend/podcast_finder/spotify_api/views.py
@@ -3,18 +3,18 @@ from .query import query
 from .tasks import search_shows
 
 #Global Variables
-OFFSET_LIMIT = 998
+OFFSET_LIMIT = 900
 
 # Create your views here.
 def search_handler(request):
     search_list = query
     for item in search_list:
         offset = 0
-        while offset<OFFSET_LIMIT:
+        while offset<=OFFSET_LIMIT:
             search_shows(item, offset)
-            offset+=1
+            offset+=50
             search_shows(item, offset)
-            offset+=1
+            offset+=50
             
     
 
